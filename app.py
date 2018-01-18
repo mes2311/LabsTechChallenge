2043f2978e1096b2992b7972976cba178c455317
mes2311 <32971193+mes2311@users.noreply.github.com>
2018-01-17 21:29:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/mes2311', methods=['GET'])
 def main():
-    return render_template('index.html')
+    return render_template('mes2311.html')
 
 
 if __name__ == '__main__':
